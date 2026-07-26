1d65e9189982c3d51c8f370c1b3cfef806c6d93b
FahimIstiaq17 <fahimistiaq17@gmail.com>
2026-07-26 19:50:13 +0600

    added toggle into button visited & notVisited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,6 +1,6 @@
 import React,{use} from 'react';
 import Country from '../Country/Country';
-
+import './Countries.css';
 const Countries = ({countriesPromise}) => {
 
     const countriesData = use(countriesPromise);
@@ -10,11 +10,15 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h2>In the country.....{countries.length} </h2>
-            {
+            
+            <div className='countries'>
+                {
                 countries.map(country => <Country 
                     key= {country.cca3.cca3}
                     country={country}> </Country>)
             }
+
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import './Country.css';
 const Country = ({ country }) => {
+    const [visited, setVisited] = React.useState(false);
     // console.log(country);
+    const handleVisited = () => {
+        // console.log('Button Clicked');
+        // mathod 1
+        // setVisited(visited ? false : true);
+
+        // mathod 2
+        setVisited(!visited);
+    }
+
     return (
-        <div>
+        <div className="country">
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common} </h2>
             <p>Population: {country.population.population} </p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? '(Large Country)' : '(Small Country)'} </p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
